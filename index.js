d04ffb1a956cec6831d4f5d217ec23b336885985
Christopher Castellano <c.castellano18@gmail.com>
2018-01-03 14:47:40 -0600

    fix

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
 app.use(session({
