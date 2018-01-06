f931f97b676807e3899d9eb59607c413606ee588
Jason Amador <jason.amador@gmail.com>
2018-01-05 19:37:11 -0600

    a few config changes for heroku

--- knexfile.js
+++ knexfile.js
@@ -27,11 +27,12 @@ module.exports = {
   },
 
   production: {
+    host: 'ec2-107-22-183-40.compute-1.amazonaws.com',
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: 'd354s1qfapm5m8',
+      user:     'afaqeruhaviicl',
+      password: '6b60c3362688c4108cb50c9803db7348f2d14386962884a96055f19bf069ffe4'
     },
     pool: {
       min: 2,
